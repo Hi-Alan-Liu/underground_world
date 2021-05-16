06fc8a149317572b18490603a17d1ab1497373f6
Alan <pa013971@gmail.com>
2021-05-16 19:23:55 +0800

    1.專案初始化 2.設定及怪物主角設定

---
+++ Assets/FantasyMonster/Skeleton/Character/Player.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    ///宣告變數
+    public float speed = 5;//角色移動速度
+    public float rotateSpeed = 5;
+    Vector3 velocity;//儲存角色位置
+
+    //儲存動畫
+    Animator anim;
+
+    // 0: 可以移動
+    // 1: 攻擊狀態
+    // 2: 被攻擊中
+    public int status = 0;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        Debug.Log(Checkd(11));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move ();
+    }
+
+    void Move ()
+    {
+
+        if (status != 0) 
+        {
+            return;
+        }
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // Debug.Log("左右數值" + h);
+        // Debug.Log("左右數值" + v);
+        //計算移動位置
+        velocity = new Vector3(0, 0, v);
+        velocity = transform.TransformDirection(velocity);
+        
+        anim.SetFloat("Speed", v);
+        if (v > 0.1)
+        {
+            //移動位置乘上速度
+            velocity *= speed;
+        }
+
+        //把計算好的位置加到角色
+        transform.localPosition += velocity * Time.fixedDeltaTime;
+        //角色旋轉
+        transform.Rotate(0, h * rotateSpeed, 0);
+        
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Fire1"))
+        {
+            anim.SetBool("Attack", true);
+            status = 1;
+        }
+    }
+
+    string Checkd(int cs) 
+    {
+        if (cs> 10)
+        {
+            return "完美";
+        }
+        else
+        {
+            return "不完美";
+        }
+    }
+
+    void AttackEnd()
+    {
+        anim.SetBool("Attack", false);
+        status = 0;
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        Debug.Log("測試碰撞");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# underground_world
+https://hackmd.io/@DfW-a1NDRXyxlnXCYWGb6Q/SJkNpigD_#
+
+# 屋塔房課程
+
+## 遊戲模型
+> [主角模型](https://assetstore.unity.com/packages/3d/characters/humanoids/humans/fantasy-chess-rpg-character-arthur-160647) 
+> [怪物模型](https://assetstore.unity.com/packages/3d/characters/humanoids/fantasy-monster-skeleton-35635)
+> [場景模型](https://assetstore.unity.com/packages/3d/environments/fantasy/fantasy-forest-environment-free-demo-35361)
+> [特效](https://assetstore.unity.com/packages/vfx/particles/cartoon-fx-free-109565)
+> [讀取](https://assetstore.unity.com/packages/2d/gui/icons/animated-loading-icons-47844)
+
+## VS CODE 套件
+> [Bracket Pair Colorizer](https://marketplace.visualstudio.com/items?itemName=CoenraadS.bracket-pair-colorizer) 
+> [indent-rainbow](https://marketplace.visualstudio.com/items?itemName=oderwat.indent-rainbow) 
+> [Debugger for Unity](https://marketplace.visualstudio.com/items?itemName=Unity.unity-debug) 
+> 
+## 主要動畫 
+* Idle、Run、Attack、Damage、Death
+
+## 主要指令
+
+**監測鍵盤**
+>  float h = Input.GetAxis("Horizontal");
+>  float v = Input.GetAxis("Vertical");
+>  Input.GetKeyDown(KeyCode.Space)
+
+**角色移動及旋轉**
+> velocity = new Vector3(0, 0, v);
+> velocity = transform.TransformDirection(velocity);
+> transform.localPosition += velocity * Time.fixedDeltaTime;
+> transform.Rotate(0, h * rotateSpeed, 0);
+
+**動畫控制及偵測**
+> `Animator`.SetBool("Attack", true);
+> `Animator`.SetFloat("Speed", 5);
+> `Animator`.GetCurrentAnimatorStateInfo(0).IsName("Run")
+
+**怪物自動尋路**
+> Vector3.Distance(transform.position,     `GameObject`.transform.position);
+> `NavMeshAgent`.SetDestination(`GameObject`.transform.position);
+
+**enum狀態**
+>enum SkeletonStatus
+> {
+>     ATTACK, // 遊戲準備
+>     MOVE, // 遊戲開始
+>     Damage   // 遊戲結束
+> }
+> SkeletonStatus gameStatus;
