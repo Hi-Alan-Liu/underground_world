0905230f4006b14bafabc44c891aa0110ff759e3
rm17945555 <s17945555@gmail.com>
2021-10-22 20:09:56 +0800

    首頁製作

---
+++ Assets/Script/Scenes.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Scenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void Level1()
+    {
+        SceneManager.LoadScene("NewLevel01");
+    }
+        public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+        public void Exit()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
