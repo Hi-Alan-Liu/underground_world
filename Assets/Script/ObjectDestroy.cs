50acad3765456d33b76aa219cb516df13cd078df
rm17945555 <s17945555@gmail.com>
2022-01-08 04:31:54 +0800

    物件破壞實現

---
+++ Assets/Script/ObjectDestroy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDestroy : MonoBehaviour
+
+{
+    // Start is called before the first frame update
+    public GameObject destroyNoise;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.tag == "Attack")
+        {
+            Debug.Log("撞門");
+            Invoke("Destroy", 5);
+        }
+    }
+    void Destroy()
+    {
+        Destroy(this.gameObject);
+    }
+    void Noise()
+    {
+        GameObject gameObject = destroyNoise;
+        Instantiate(gameObject, new Vector3(transform.position.x,transform.position.y+2f,transform.position.z),transform.rotation);
+    }
+}
