e0b995d8e2a66d33c77108b8c073090d8f476410
pa013971 <pa013971@gmail.com>
2022-04-09 16:31:28 +0800

    新增首頁選擇角色效果

---
+++ Assets/Script/SelectPlayer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectPlayer : MonoBehaviour
+{
+    public GameObject panel_1;
+
+    public GameObject panel_2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PointerEnter(int id)
+    {
+        if (id == 1) {
+            panel_1.SetActive(false);
+        } else {
+            panel_2.SetActive(false);
+        }
+    }
+
+    public void PointerExit(int id)
+    {
+        if (id == 1) {
+            panel_1.SetActive(true);
+        } else {
+            panel_2.SetActive(true);
+        }
+    }
+
+    public void PointerClick(int id)
+    {
+
+    }
+}
